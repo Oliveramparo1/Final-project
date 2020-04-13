a1aa90d1f066eb81aaabaf9c5c89adc68e9e5d84
Oliveramparo1 <63202907+Oliveramparo1@users.noreply.github.com>
2020-04-12 21:40:50 -0400

    ToDo Readme

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Hello")
+import pygame
+
+pygame.init()
+
+weigh = 800
+high = 500
+pygame.display.set_mode((weigh, high))
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, image):
+        super().__init__()
+        self.image = image
+        self.rect = self.image.loud('Run(1).png')
+
+
+
+if __name__ == "__main__":
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            main = False
+    pygame.display.update()
+
