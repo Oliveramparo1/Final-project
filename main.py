d40a07f978fd547c3d1ebdc57f505fe4ec44b8b8
Oliveramparo1 <63202907+Oliveramparo1@users.noreply.github.com>
2020-04-13 20:14:24 -0400

    ToDo Readme

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
 import pygame
+import os
 
 pygame.init()
-
-weigh = 800
-high = 500
-pygame.display.set_mode((weigh, high))
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, image):
-        super().__init__()
-        self.image = image
-        self.rect = self.image.loud('Run(1).png')
-
+Screen_weigh = 800
+Screen_high = 500
+screen = pygame.display.set_mode((Screen_weigh, Screen_high))
+pygame.display.set_caption("Running")
+x = 50
+y = 400
+width = 40
+hight = 60
+velocity = 5
 
 
-if __name__ == "__main__":
+def images():
+    os.path.abspath('D:/Final-project/png')
+    walkright = pygame.image.load('Run(1).png')
+
+
+def movement():
+    global x
+    global y
+    key_pressed = pygame.key.get_pressed()
+    if key_pressed[pygame.K_d] and x < Screen_weigh - 40:
+        x = x + velocity
+    if key_pressed[pygame.K_a] and x > 0:
+        x = x - velocity
+    if key_pressed[pygame.K_w] and y > 0:
+        y = y - velocity
+    screen.fill((255, 0, 0))
+
+
+thing = True
+while thing:
+    pygame.time.delay(100)
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
-            main = False
+            thing = False
+    movement()
+    pygame.draw.rect(screen, (0, 255, 0), (x, y, width, hight))
     pygame.display.update()
-
+pygame.quit()
